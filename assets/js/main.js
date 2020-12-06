1104b2d0d7593805f9f5cb4b870fb3ab01cd08e7
Blattinum <collin.blatt@gmail.com>
2020-12-06 04:19:32 -0500

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,5 @@ let newDate = new Date();
 let year = newDate.getFullYear();
 
 //insert and modify header and footer
-document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", `<header><div class="title"><h1><a href="` + url + `">SPECTRE <img src="./assets/img/spectre-v4.svg" width="50" height="50"></a></h1></div></header>`);
+document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", `<header><div class="title"><h1><a href="` + url + `"><img src="./assets/img/spectre-v4.svg" width="60" height="60"><br>SPECTRE</a></h1></div></header>`);
 document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", `<footer><div  class="copyright"><a href="` + url + `">JAMES BOND IS THE PROPERTY OF IAN FLEMING PUBLICATIONS, WITH WHICH WE HAVE NO AFFILIATION. ALL RIGHTS RESERVED BY THEIR RESPECTIVE OWNERS.</a></div></footer>`);
