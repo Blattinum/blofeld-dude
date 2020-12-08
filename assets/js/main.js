014bae28cc162f11a4b7b8e8bce8da486ae733e5
blattinum <collin.blatt@gmail.com>
2020-12-08 16:35:53 -0500

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,9 @@ let year = newDate.getFullYear();
 //insert and modify header and footer
 document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", `<div id="top"></div><header><div class="title"><img src="./assets/img/spectre-v4.svg" width="50" height="50"><br><h1><a href="` + url + `">SPECTRE</a></h1></div></header>`);
 document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", `<footer><div  class="copyright"><a href="` + url + `">JAMES BOND IS THE PROPERTY OF IAN FLEMING PUBLICATIONS, ET AL.&mdash;WITH WHICH WE HAVE NO AFFILIATION. ALL RIGHTS RESERVED BY THEIR RESPECTIVE OWNERS.</a></div></footer>`);
+
+
+window.addEventListener("load",() =>
+  [...document.querySelectorAll("a[target=_blank]")]
+    .forEach(lnk => lnk.setAttribute("rel", "noopener noreferrer"))
+);
