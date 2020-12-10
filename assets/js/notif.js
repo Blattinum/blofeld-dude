2e3c79a2dfdd5dbe7830fdfa7421c1c7d3a6928a
blattinum <collin.blatt@gmail.com>
2020-12-10 16:33:12 -0500

    Update notif.js

--- assets/js/notif.js
+++ assets/js/notif.js
@@ -2,7 +2,7 @@
  * @Author: Collin Blatt 
  * @Date: 2020-12-10 16:24:53 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2020-12-10 16:31:47
+ * @Last Modified time: 2020-12-10 16:33:08
  */
 
 let d = new Date();
@@ -67,7 +67,7 @@ function displayNotification() {
                 }
             };
             reg.showNotification(day + '.' + month + '.' + year, options);
-            navigator.vibrate([500]);
+            navigator.vibrate([50, 25, 50]);
         });
     }
 }
