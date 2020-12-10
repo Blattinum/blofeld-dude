1ad11701c7cd81fda6b2fe33f07e16e3bcf49178
Blattinum <collin.blatt@gmail.com>
2020-12-09 23:19:38 -0500

    Update notif.js

--- assets/js/notif.js
+++ assets/js/notif.js
@@ -6,10 +6,21 @@ Notification.requestPermission(function (status) {
     console.log('Notification permission status:', status);
 });
 
+// BEGINNING OF FUNCTIONS
+
 function displayNotification() {
     if (Notification.permission == 'granted') {
         navigator.serviceWorker.getRegistration().then(function (reg) {
-            reg.showNotification('Hello world!');
+            var options = {
+                body: 'Here is a notification body!',
+                icon: '../../android-chrome-512x512.png',
+                vibrate: [100, 50, 100],
+                data: {
+                    dateOfArrival: Date.now(),
+                    primaryKey: 1
+                }
+            };
+            reg.showNotification('Hello world!', options);
         });
     }
 }
